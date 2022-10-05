7c8ea4df043023de17151c8dd9081c942e991741
SreeRaj10 <107820079+SreeRaj10@users.noreply.github.com>
2022-10-05 15:30:00 +1100

    Update objects.js

--- objects.js
+++ objects.js
@@ -31,27 +31,13 @@ let wardrobe =[];
 
 wardrobe.shirt();
 // 3. Create an empty array assigned to a variable called wardrobe.
-// This should contain all of your shirts.
-// Create three new shirt objects with different colours and sizes
-// Push the shirt objects to an array
+const wardrobe = [];
+  wardrobe.push(shirt);
+  wardrobe.push(shirt2);
+  wardrobe.push(shirt3);
 
 
 // 4. (bit of a challenge!) Create a for loop that goes through the entire wardrobe array
-// Each time it loops through, it should look at the shirt array item it is up to
-// and console.log the colour of the shirt.
-
-
-
-
-
-//   // 3-----------------
-//   const wardrobe = [];
-
-//   wardrobe.push(shirt);
-//   wardrobe.push(shirt2);
-//   wardrobe.push(shirt3);
-
-//   // 4-----------------
-//   for (i=0; i < wardrobe.length; i++) {
-//       console.log(wardrobe[i].colour);
-//   }
+for (i=0; i < wardrobe.length; i++) {
+console.log(wardrobe[i].colour);
+ }
